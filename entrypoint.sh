f1710f04fc19afa2ebc7b463bb86735ff721004d
Irfandi Iqbal Abimanyu <irfndiabimanyu@gmail.com>
2026-05-02 13:58:44 +0700

    fix: update certificate paths in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,13 @@
 set -e
 
 # Add certificate files
-echo "$SERVER_CRT" > /etc/dokploy/certs/postgres/server.crt
-echo "$SERVER_KEY" > /etc/dokploy/certs/postgres/server.key
-echo "$ROOT_CA_CRT" > /etc/dokploy/certs/postgres/ca.crt
+echo "$SERVER_CRT" > /var/lib/postgresql/server.crt
+echo "$SERVER_KEY" > /var/lib/postgresql/server.key
+echo "$ROOT_CA_CRT" > /var/lib/postgresql/ca.crt
 
 # Update file permissions of certificates
-chmod 600 /etc/dokploy/certs/postgres/server.* /etc/dokploy/certs/postgres/ca.crt
-chown postgres:postgres /etc/dokploy/certs/postgres/server.* /etc/dokploy/certs/postgres/ca.crt
+chmod 600 /var/lib/postgresql/server.* /var/lib/postgresql/ca.crt
+chown postgres:postgres /var/lib/postgresql/server.* /var/lib/postgresql/ca.crt
 
 # Run the base entrypoint
-docker-entrypoint.sh postgres
+docker-entrypoint.sh postgres 
